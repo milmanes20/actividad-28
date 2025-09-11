fdc143f4d943cf4ef3436c6ae90a2781032840ef
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2025-09-11 08:37:54 -0300

    mod ficha personal

--- src/ActorCard.jsx
+++ src/ActorCard.jsx
@@ -15,27 +15,27 @@ function ActorCard({ name, lastName, age, nationality, movies, education, bio, i
         <img src={image} alt={`${name} ${lastName}`} className="actor-image" />
 
         <div className="info">
-          <h3>Personal information:</h3>
-          <p><b>Name:</b> {name}</p>
-          <p><b>Last Name:</b> {lastName}</p>
-          <p><b>Age:</b> {age}</p>
-          <p><b>Nationality:</b> {nationality}</p>
+          <h3>Informacion Personal:</h3>
+          <p><b>Nombre:</b> {name}</p>
+          <p><b>Apeliido:</b> {lastName}</p>
+          <p><b>Edad:</b> {age}</p>
+          <p><b>Nacionalidad:</b> {nationality}</p>
 
-          <h3>Experience in movies:</h3>
+          <h3>Experiencia en peliculas:</h3>
           <ul>
             {movies.map((movie, index) => (
               <li key={index}>{movie}</li>
             ))}
           </ul>
 
-          <h3>Education:</h3>
+          <h3>Educación:</h3>
           <ul>
             {education.map((edu, index) => (
               <li key={index}>{edu}</li>
             ))}
           </ul>
 
-          <h3>Bio:</h3>
+          <h3>Biografía:</h3>
           <p>{bio}</p>
         </div>
       </div>
